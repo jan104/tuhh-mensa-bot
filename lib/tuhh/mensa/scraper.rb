0f42973bfb6fe48c738f9fed2c5f24bfbefe926a
Martin Borchert <martin-anon+tuhh-mensa-bot@posteo.de>
2019-01-16 15:53:58 +0100

    Refactor in order to improve encapsulation

--- lib/tuhh/mensa/scraper.rb
+++ lib/tuhh/mensa/scraper.rb
@@ -12,37 +12,6 @@ class TUHH::Mensa::Scraper
     @icons = load_icons
   end
 
-  def make_url(spec, lang)
-    url = @config.dig(:urls, lang)
-    year = Time.now.year
-
-    case spec
-    when :now
-      url + "/#{year}/0/"
-    when :next
-      url + "/#{year}/99/"
-    end
-  end
-
-  def fetch(spec, lang)
-    url = make_url(spec, lang)
-    [url, HTTP.get(url).to_s] # TODO: Error handling
-  end
-
-  def map_icon(img)
-    alt = img.attr("alt")
-    @icons.each { |lang, icons|
-      icons.each { |desc, emoji|
-        re = /#{Regexp.quote(desc)}/i
-        if alt =~ re
-          return emoji
-        end
-      }
-    }
-
-    "X"
-  end
-
   def scrape(spec, lang)
     url, html = fetch(spec, lang)
     dom = Nokogiri::HTML(html)
@@ -74,5 +43,36 @@ class TUHH::Mensa::Scraper
   private
   def load_icons
     Psych.load_file(File.expand_path("../scraper/icons.yaml", __FILE__))
+  end
+
+  def make_url(spec, lang)
+    url = @config.dig(:urls, lang)
+    year = Time.now.year
+
+    case spec
+    when :now
+      url + "/#{year}/0/"
+    when :next
+      url + "/#{year}/99/"
+    end
+  end
+
+  def fetch(spec, lang)
+    url = make_url(spec, lang)
+    [url, HTTP.get(url).to_s] # TODO: Error handling
+  end
+
+  def map_icon(img)
+    alt = img.attr("alt")
+    @icons.each { |lang, icons|
+      icons.each { |desc, emoji|
+        re = /#{Regexp.quote(desc)}/i
+        if alt =~ re
+          return emoji
+        end
+      }
+    }
+
+    "X"
   end
 end
